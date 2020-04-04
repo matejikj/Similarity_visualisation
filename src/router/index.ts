60531696a5ee6a5a8a011199f93e69eb034b319a
matejikj <jakub.matejik@centrum.cz>
2020-04-04 14:10:33 +0200

    Tutorial as view

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,14 @@ const routes = [
     path: '/',
     name: 'Visualisation',
     component: Visualisation
+  },
+  {
+    path: '/tutorial',
+    name: 'Tutorial',
+    // route level code-splitting
+    // this generates a separate chunk (tutorial.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tutorial" */ '../views/Tutorial.vue')
   }
 ]
 
